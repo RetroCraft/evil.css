ea381acefe87cfcfb43f033fde4b90dcb5e2d971
James <RetroCraft@users.noreply.github.com>
2015-03-31 12:29:44 -0400

    Update evil-safari-mac.sh

--- evil-safari-mac.sh
+++ evil-safari-mac.sh
@@ -3,13 +3,17 @@
 base="$HOME/Library/Application Support/Safari"
 file="$base/evil.css"
 
+echo "Downloading evil.css..."
 mkdir -p "$base"
 curl -L 'https://github.com/RetroCraft/evil.css/raw/master/evil.css' > "$file"
-
+echo "Download complete... killing Safari..."
 osascript -e 'tell application "Safari"' -e 'quit' -e 'end tell'
 sleep 1
-
+echo "Setting defaults values..."
+defaults write "com.apple.Safari" "UserStyleSheetEnabled" "1"
+defaults write "com.apple.Safari" "UserStyleSheetLocationURLString" "$file"
 defaults write "com.apple.Safari" "WebKitUserStyleSheetLocationPreferenceKey" "$file"
 defaults write "com.apple.Safari" "WebKitUserStyleSheetEnabledPreferenceKey" "1"
-
+echo "Restarting Safari..."
 open -a "Safari" "https://github.com/RetroCraft/evil.css"
+echo "ALL DONE!"
