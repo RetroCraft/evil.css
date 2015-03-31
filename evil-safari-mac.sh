0e99c6006548c9ce7b867d93573a24a22fc16d40
James <RetroCraft@users.noreply.github.com>
2015-03-31 11:15:41 -0400

    Update evil-safari-mac.sh

--- evil-safari-mac.sh
+++ evil-safari-mac.sh
@@ -4,7 +4,7 @@ base="$HOME/Library/Application Support/Safari"
 file="$base/evil.css"
 
 mkdir -p "$base"
-curl -L 'https://github.com/tlrobinson/evil.css/raw/master/evil.css' > "$file"
+curl -L 'https://github.com/RetroCraft/evil.css/raw/master/evil.css' > "$file"
 
 osascript -e 'tell application "Safari"' -e 'quit' -e 'end tell'
 sleep 1
@@ -12,4 +12,4 @@ sleep 1
 defaults write "com.apple.Safari" "WebKitUserStyleSheetLocationPreferenceKey" "$file"
 defaults write "com.apple.Safari" "WebKitUserStyleSheetEnabledPreferenceKey" "1"
 
-open -a "Safari" "https://github.com/tlrobinson/evil.css"
+open -a "Safari" "https://github.com/RetroCraft/evil.css"
